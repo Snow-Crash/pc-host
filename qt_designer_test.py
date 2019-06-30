d3a250f0059bced534b340a007fe4285fd968761
Snow-Crash <hfang02@syr.edu>
2019-06-30 18:58:56 -0400

    visual improvements

--- qt_designer_test.py
+++ qt_designer_test.py
@@ -52,6 +52,13 @@ WINDOW = 450
 SYNAPSES = 110
 NEURONS = 10
 PATTERNS = 10
+SLIDING_WINDOW = 100
+DATA_PLOT_RANGE = WINDOW
+
+USE_SLIDING_WINDOW = True
+
+if USE_SLIDING_WINDOW:
+    DATA_PLOT_RANGE = SLIDING_WINDOW
 
 PREPROCESSED_SPIKE_LOCATION = 'D:/islped_demo/snn/noise_train.npy'
 RAW_DATA_LOCATION = 'D:/islped_demo/snn/interpolated_raw_data.npy'
@@ -87,8 +94,37 @@ tick_font = QtGui.QFont('Microsoft YaHei', 8)
 colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728',
               '#9467bd', '#8c564b', '#e377c2', '#7f7f7f',
               '#bcbd22', '#17becf']
+
+#
+banner_logo_img = QtGui.QPixmap('./images/eng_logo_2.png')
+window.banner_logo.setPixmap(banner_logo_img.scaled(QtCore.QSize(250, 300),
+                                                    QtCore.Qt.KeepAspectRatio))
+
+#window.buttom_line.setFrameShape(QtGui.QFrame.HLine);
+#window.buttom_line.setFrameShadow(QtGui.QFrame.Plain);
+#window.buttom_line.setLineWidth(2)
+              
+## Create some widgets to be placed inside
+              
+su_logo_background = QtGui.QPixmap('./images/su_logo_white.png')
+window.logo.setPixmap(su_logo_background.scaled(QtCore.QSize(150, 150)))
+window.logo.setAlignment(QtCore.Qt.AlignCenter)
+
+
+window.title_label.setWordWrap(True)
+window.title_label.setFont(font)
+              
+btn_background = QtGui.QPixmap('./images/start2.png')
 ## Create some widgets to be placed inside
 window.btn.setFont(font)
+window.btn.setIcon(QtGui.QIcon(btn_background))
+window.btn.setIconSize(QtCore.QSize(32, 32));
+window.btn.setAutoRaise(True)
+
+#if button type is qtool botto, text can be place under 
+#reference: https://www.riverbankcomputing.com/static/Docs/PyQt4/qt.html#ToolButtonStyle-enum
+window.btn.setToolButtonStyle(QtCore.Qt.ToolButtonTextBesideIcon);
+
 window.auto_check_box.setFont(font)
 window.input_class_label.setFont(font)
 window.dropdown_patterns.setFont(font)
@@ -101,7 +137,7 @@ window.dropdown_patterns.setItems(dropdown_options_dict())
 raw_data_lines = []
 #insert lines to container
 for i in range(DATA_SELECT_SYNAPSES):
-    line = window.raw_data_plot.plot(np.random.rand(WINDOW), pen=colors[i%len(colors)])
+    line = window.raw_data_plot.plot(np.random.rand(DATA_PLOT_RANGE), pen=colors[i%len(colors)])
     raw_data_lines.append(line)
 window.raw_data_plot.plotItem.setTitle('Input stimulus')
 window.raw_data_plot.plotItem.titleLabel.item.setFont(font)
@@ -123,12 +159,13 @@ for i in range(DATA_SELECT_SYNAPSES):
 window.voltage_plot.plotItem.titleLabel.item.setFont(font)
 window.voltage_plot.getAxis("left").tickFont = tick_font
 window.voltage_plot.getAxis("bottom").tickFont = tick_font
+window.voltage_plot.plotItem.setYRange(-5,1.5)
 
 voltage_lines = []
 for i in range(DATA_SELECT_NEURONS):
-    line = window.voltage_plot.plot(np.random.rand(WINDOW), pen=colors[i%len(colors)])
+    line = window.voltage_plot.plot(np.random.rand(DATA_PLOT_RANGE), pen=colors[i%len(colors)])
     voltage_lines.append(line)
-window.voltage_plot.plotItem.setTitle('Membrane Potential')
+# window.voltage_plot.plotItem.setTitle('Membrane Potential')
 
 
 window.inspike_plot.plotItem.titleLabel.item.setFont(font)
@@ -166,12 +203,8 @@ def classid2sampleidx(drop_down_value):
 
 
 def send_recieve_packages():
-    # voltage = np.zeros([10,WINDOW])
-    # psp = np.zeros([110,WINDOW])
     
     start = timer()
-#    global current_pattern # Allows for communication between Comm. Loop and Plotting Loop
-#    global selected_sample_idx
     current_pattern = window.dropdown_patterns.value()
     while True:
         selected_sample_idx = classid2sampleidx(current_pattern)
@@ -192,8 +225,10 @@ def send_recieve_packages():
             for n in range(s.__len__()):
                 if s[n] > 0:
                      window.listw.insertItem(0, "Spike Detected! Neuron: {}, Step: {} ".format(n, t))
+            # Reset psp at last step
             if t == WINDOW - 1:
                 controller.reset_neuron()
+
         window.listw.insertItem(0, "End Pattern: " + current_pattern.__str__())
         if not window.auto_check_box.checkState():
             break
@@ -201,7 +236,6 @@ def send_recieve_packages():
         time.sleep(3.0)
         current_pattern = np.random.randint(0, high=PATTERNS)
         selected_sample_idx = classid2sampleidx(current_pattern)
-#        print(selected_sample_idx)
 
         
     print(timer() - start)
@@ -215,16 +249,13 @@ def btn_clicked_function():
 
 def update():
     
-#    global current_pattern # Allows for communication between Comm. Loop and Plotting Loop
-#    global selected_sample_idx
-
-    
-    voltage = np.zeros([NEURONS, WINDOW])
+    voltage = np.zeros([NEURONS, DATA_PLOT_RANGE])
     psp = np.zeros([SYNAPSES, WINDOW])
     out_spikes = np.zeros([NEURONS, WINDOW])
-    raw_data = np.zeros([22, WINDOW])
+    raw_data = np.zeros([22, DATA_PLOT_RANGE])
     last_t = 0
     current_t = 0
+    ptr = -SLIDING_WINDOW
     while True:
         while not info_queue.empty():
             current_pattern, selected_sample_idx = info_queue.get()
@@ -235,7 +266,20 @@ def update():
             merge_data = ploting_queue.get()
             s, p, v, t = merge_data
             v_record[0, :, t] = v
-            voltage[:, t] = v
+            ptr += 1
+            if USE_SLIDING_WINDOW:
+                voltage[:,0:-1] = voltage[:,1:]
+#                psp[:,0:-1] = psp[:,1:]
+                raw_data[:, 0:-1] = raw_data[:,1:]
+                
+                voltage[:, -1] = v
+#                psp[:, -1] = p
+                raw_data[:, -1] = raw_input[selected_sample_idx, :, t]
+            else:
+                voltage[:, t] = v
+#                psp[:, t] = p
+                raw_data[:, :t] = raw_input[selected_sample_idx, :, :t]
+
             psp[:, t] = p
             out_spikes[:, t] = s
             current_t = t
@@ -246,41 +290,30 @@ def update():
 #                in_scatter.clear()
 #                out_scatter.clear()
                 
-            # Reset psp at last step
-            if (t == 450-1):
-                
-                pass
             
             #reset all data at the begining of each run
             if t == 0:
-                pass
-                voltage = np.zeros([NEURONS, WINDOW])
-                psp = np.zeros([SYNAPSES, WINDOW])
                 out_spikes = np.zeros([NEURONS, WINDOW])
-                raw_data = np.zeros([22, WINDOW])
-#                for i in range(DATA_SELECT_SYNAPSES):
-#                    raw_data_lines[i].setData(spike[current_pattern, 0:current_t, i] + i * 1000)
-#                    psp_lines[i].setData(psp[::5][i] + i)
-        
-            raw_data[:, :t] = raw_input[selected_sample_idx, :, :t]
+                voltage = np.zeros([NEURONS, DATA_PLOT_RANGE])
+                psp = np.zeros([SYNAPSES, WINDOW])
+                raw_data = np.zeros([22, DATA_PLOT_RANGE])
+                ptr = -SLIDING_WINDOW
+
 
         # Synapse Plotting
         x = []
         y = []
         sc = 0
         for i, syn_idx in enumerate(selected_synapses_idx):
-#        for i in range(DATA_SELECT_SYNAPSES):
-#        for i,idx in enumerate(selected_synapse):
             # use setdata instead of plot. setdata is faster
             # raw_data_lines[i].setData(np.random.random(WINDOW) + i)
-#            raw_data_lines[i].setData(spike[current_pattern, 0:current_t, i] + i * 1000)
-#            print("psp shape", psp.shape)
-#            print("psp[::5] shape", psp[::5].shape)
             raw_data_lines[i].setData(raw_data[i])
             psp_lines[i].setData(psp[::5][i] + i)
-#            print(i, idx)
-            # Plotting Input Spikes
-#        for i in range(SYNAPSES):
+
+            if USE_SLIDING_WINDOW:
+#                psp_lines[i].setPos(ptr, 0)
+                raw_data_lines[i].setPos(ptr, 0)
+
             if current_t > last_t:
                 for step in range(last_t, current_t):
                     if spike[selected_sample_idx, step, syn_idx] > 0:
